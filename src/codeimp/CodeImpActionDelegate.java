1fcd2328cb7b834dbe651232cf27a5828c45734c
chuxuankhoi <khoi.chu@ucdconnect.ie>
2014-06-05 20:15:47 +0100

    Delete unused imports

--- src/codeimp/CodeImpActionDelegate.java
+++ src/codeimp/CodeImpActionDelegate.java
@@ -5,11 +5,7 @@ package codeimp;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.text.TextSelection;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.ISelectionProvider;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchPartSite;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
