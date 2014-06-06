8e9813082febac0893570e5b1118d4e66db5b8bb
chuxuankhoi <khoi.chu@ucdconnect.ie>
2014-06-06 10:37:22 +0100

    Refactor the function run() Complete getting selected text

--- src/codeimp/CodeImpActionDelegate.java
+++ src/codeimp/CodeImpActionDelegate.java
@@ -5,7 +5,11 @@ package codeimp;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.text.TextSelection;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.ui.IWorkbenchPartSite;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
@@ -26,19 +30,23 @@ public class CodeImpActionDelegate implements IWorkbenchWindowActionDelegate {
 		MessageDialog.openInformation(window.getShell(),
 				"Have not completed yet", "CodeImp is under construction");
 
-		// TODO Check Java perspective and editor
-		String curPerspective = window.getActivePage().getPerspective()
-				.getLabel();
-		if (curPerspective.equals("Java") == false) {
+		// Check Java perspective and editor
+		if (!isPerspective("Java")) {
 			MessageDialog.openInformation(window.getShell(),
 					"Checking current perspective ...",
-					"Current version have not support the language.\nCurrent perspective: "
-							+ curPerspective);
+					"Current version have not support the language.");
 			return;
 		}
 
-		// TODO Get the selected code in the current editor
-		
+		// Get the selected code in the current editor
+		String selectedCode = getSelectedCode();
+		if (selectedCode.isEmpty()) {
+			MessageDialog.openInformation(window.getShell(),
+					"Getting selected text ...", "No text is selected.");
+			return;
+		}
+		System.out.println(selectedCode);
+
 		// TODO Analyse code smell
 
 		// TODO Find appropriate improvement for each issue
@@ -46,6 +54,46 @@ public class CodeImpActionDelegate implements IWorkbenchWindowActionDelegate {
 		// TODO Display the analysis and suggestion
 
 		// TODO Change the current code as suggested if user accept the solution
+	}
+
+	private boolean isPerspective(String expectedPerspective) {
+		String curPerspective = window.getActivePage().getPerspective()
+				.getLabel();
+		if (curPerspective.equals(expectedPerspective)) {
+			return true;
+		}
+		return false;
+	}
+	
+	private String getSelectedCode() {
+		IWorkbenchPartSite site = window.getPartService().getActivePart()
+				.getSite();
+		if (site == null) {
+			MessageDialog.openInformation(window.getShell(),
+					"Getting selected text ...", "No workbench part site.");
+			return "";
+		}
+		ISelectionProvider provider;
+		ISelection selection;
+		provider = site.getSelectionProvider();
+		if (provider == null) {
+			MessageDialog.openInformation(window.getShell(),
+					"Getting selected text ...", "No selection provider.");
+			return "";
+		}
+		selection = provider.getSelection();
+		if (selection == null) {
+			MessageDialog.openInformation(window.getShell(),
+					"Getting selected text ...", "No selection is get.");
+			return "";
+		}
+		if(!(selection instanceof ITextSelection)) {
+			MessageDialog.openInformation(window.getShell(),
+					"Getting selected text ...", "No text selection is get.");
+			return "";
+		}
+		ITextSelection ts = (ITextSelection) selection;
+		return ts.getText();
 	}
 
 	/*
